510a1b24c317bf02167ddd8eb2e758aadc8993e3
victorgis <essangvictor@gmail.com>
2023-01-15 15:40:03 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css"
+
+import Header from "./components/Header";
+import Leaflet from "./components/Leaflet";
+
+
+function App() {
+  return (
+    <div>
+      {/* <Header /> */}
+      <Leaflet />
+    </div>
+  );
+}
+
+export default App;
