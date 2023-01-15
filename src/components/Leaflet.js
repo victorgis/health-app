24bfb5a092705302e68e9567b202ee80c87f3abb
victorgis <essangvictor@gmail.com>
2023-01-15 16:16:23 +0100

    added leaflet map

--- src/components/Leaflet.js
+++ src/components/Leaflet.js
@@ -1,45 +1,27 @@
 // --- (1), (2) & (3): install and import ---
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { Icon } from 'leaflet';
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// import { Icon } from 'leaflet';
+import React from 'react'
+import * as L from 'leaflet'
 import 'leaflet/dist/leaflet.css';
 import './Header.css'
-// --- ---------------------------------- ---
 
-export default function Leaflet() {
-  // Berlin coordinates
-  const position = [52.51, 13.38]
+const Leaflet = () => {
+    var map = L.map('map').setView([51.505, -0.09], 13);
+    
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
-  // --- (6) Create a custom marker ---
-  const customIcon = new Icon({
-    iconUrl: '/icons8-select-24.png',
-    iconSize: [20, 20],
-    // iconAnchor: [1, 1],
-    // popupAnchor: [-0, -76]
-  })
+    var marker = L.marker([51.5, -0.09]).addTo(map);
 
-  return (
-    <section className='map-component' >
-      {/* --- (5) Add leaflet map container --- */}
-      <div className='map'>
-      <MapContainer center={position} zoom={6} scrollWheelZoom={true}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          // --- (7) Alternative map style (attribution and url copied from the leaflet extras website) ---
-          // attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-          // url='https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
-          // --- -------------------------------------------------------------------------------------- ---
-        />
-        <Marker position={position}
-          icon={customIcon}
-        >
-          <Popup>
-            🐻🍻🎉
-          </Popup>
-        </Marker>
-      </MapContainer>
-      {/* --- ---------------------------- --- */}
-      </div>
-    </section>
-  )
+    return (
+        <div id="map"></div>
+    )
+
 }
+
+export default Leaflet
+
+
